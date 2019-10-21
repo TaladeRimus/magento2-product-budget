6dd1f6482c001010517ecaf2e3976d5a49f3fad6
Adriano Casarim Duarte <ACDUARTE@senacrs.com.br>
2019-10-21 08:58:40 -0300

    Readme e remoção de comentario JS

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Este é um módulo Magento onde você cria um  produto do tipo 'budget' no admin da loja.
 Em sua loja, quando este produto aparecer, o preço e o botão de adicionar ao carrinho desaparecem, e em seu lugar um botão 'Ligue para orçamento' aparece, abrindo uma modal para contato com a loja.
+
+# Como configurar
+
+Após clonar o repositório e realizar a instalação do Magento, vá até o Admin do site e configure a extensão MAGEPLAZA/SMTP para realizar o envio de e-mail.
+Crie um produto do tipo 'budget'.
+Vá até o front e procure pelo produto para ver as alterações realizadas.
--- app/code/MageProductBudget/ProductBudget/view/frontend/web/js/callforprice-component.js
+++ app/code/MageProductBudget/ProductBudget/view/frontend/web/js/callforprice-component.js
@@ -1,75 +1,48 @@
-/*require(
+require(
     [
         'jquery',
         'Magento_Ui/js/modal/modal'
     ],
-    function(
-        $,
-        modal
-    ) {
-        var options = {
-            type: 'popup',
-            responsive: true,
-            innerScroll: true,
-            buttons: [{
-                text: $.mage.__('Continue'),
-                class: 'mymodal1',
-                click: function () {
-                    this.closeModal();
-                }
-            }]
-        };
-        var popup = modal(options, $('#myModal'));
-        $("#popupButton").on('click',function(){
-            $("#myModal").modal("openModal");
-        });
-    });*/
+    function($, modal) {
+        jQuery(document).ready(function(){
+            var options = {
+                type: 'popup',
+                responsive: true,
+                innerScroll: true,
+                title: 'Contact Form',
+                buttons: [{
+                    text: $.mage.__('Close'),
+                    class: '',
+                    click: function () {
+                        this.closeModal();
+                    }
+                }]
+            };
+            var openModal = modal(options, $('#myModal'));
 
-    require(
-        [
-            'jquery',
-            'Magento_Ui/js/modal/modal'
-        ],
-        function($, modal) {
-            jQuery(document).ready(function(){
-                var options = {
-                    type: 'popup',
-                    responsive: true,
-                    innerScroll: true,
-                    title: 'Contact Form',
-                    buttons: [{
-                        text: $.mage.__('Close'),
-                        class: '',
-                        click: function () {
-                            this.closeModal();
-                        }
-                    }]
-                };
-                var openModal = modal(options, $('#myModal'));
-    
-                jQuery('body').on('click', '.open-contact-form', function(){
-                    jQuery('.contact-form-popup').show();
-                    //jQuery('.static-block-message').hide();
-                    jQuery('#myModal').modal('openModal');
-                });
-    
-                jQuery('body').on('click', '.submit', function(e){
-                    e.preventDefault();
-                    e.stopImmediatePropagation();
-                    jQuery.ajax({
-                        type: 'post',
-                        url: 'contact/index/post',
-                        data: jQuery('#contact-form').serialize(),
-                        cache: false,
-                        showLoader: 'true',
-                        success: function(response) {
-                            alert('success');
-                            jQuery('.contact-form-popup').hide();
-                            //jQuery('.static-block-message').show();
-                        }
-                    });
-                    return false;
-                });
+            jQuery('body').on('click', '.open-contact-form', function(){
+                jQuery('.contact-form-popup').show();
+                //jQuery('.static-block-message').hide();
+                jQuery('#myModal').modal('openModal');
             });
-        }
-    );
+
+            jQuery('body').on('click', '.submit', function(e){
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                jQuery.ajax({
+                    type: 'post',
+                    url: 'contact/index/post',
+                    data: jQuery('#contact-form').serialize(),
+                    cache: false,
+                    showLoader: 'true',
+                    success: function(response) {
+                        alert('success');
+                        jQuery('.contact-form-popup').hide();
+                        //jQuery('.static-block-message').show();
+                    }
+                });
+                return false;
+            });
+        });
+    }
+);
